2c69910d4fb0817eae7aa26d472bf2605693a617
Augustus Trillanes <45346263+atrillanes1972@users.noreply.github.com>
2023-03-07 02:48:28 +0800

    Complete working code. Image detection works with email sending for the alerts and sends a sample snapshot.

--- emailing.py
+++ emailing.py
@@ -1,2 +1,27 @@
-def send_email():
-    print("Email was sent!")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD = "<PASSWORD here>"
+SENDER = "<sender email here>"
+RECEIVER = "<receiver email here>"
+def send_email(image_path):
+    #print("Email was sent!")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just had  a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/31.png")
+
